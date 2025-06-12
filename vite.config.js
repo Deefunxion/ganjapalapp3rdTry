5f0c8b63c49b0c2ee03a6b2a7998de36fe05049b
Dimitris Papadopoulos <deefunxion@gmail.com>
2025-06-12 22:04:16 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GanjapalApp3rdTry/",
+  base: "/GanjapalApp/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
